ecd8ba2b77c85bedc070803e10ea2fbb3f96856a
mariomui-viscira <mariomui.viscira@gmail.com>
2022-03-01 00:27:17 -0800

    Upgrade the automation to fit with python best practices.

--- poetry_demo/scripts/task_test.py
+++ poetry_demo/scripts/task_test.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import call, check_call, run, PIPE
 
 
 import os
@@ -8,4 +8,17 @@ BASH_SCRIPTS_DIR = "bash_scripts"
 
 
 def cli():
-    called = call('/'.join([TASK_ROOT_DIR, BASH_SCRIPTS_DIR, 'task_test.sh']))
+    task_test = '/'.join([TASK_ROOT_DIR, BASH_SCRIPTS_DIR, 'task_test.sh'])
+    # called = check_call(task_test)
+    run(['sh', task_test], check=True)
+
+
+# https://stackoverflow.com/questions/4256107/running-bash-commands-in-python
+'''
+Prefer subprocess.run() over subprocess.check_call() and friends over subprocess.call() over subprocess.Popen() over os.system() over os.popen()
+Understand and probably use text=True, aka universal_newlines=True.
+Understand the meaning of shell=True or shell=False and how it changes quoting and the availability of shell conveniences.
+Understand differences between sh and Bash
+Understand how a subprocess is separate from its parent, and generally cannot change the parent.
+Avoid running the Python interpreter as a subprocess of Python.
+'''
